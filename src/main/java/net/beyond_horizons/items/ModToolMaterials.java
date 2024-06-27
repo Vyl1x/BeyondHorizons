52f60d85799c4d5bf1df7d0f44e42bfd8d6ebbb7
Vyl1x <alekdimov2003@icloud.com>
2024-06-27 22:48:43 +0300

    Fixed tool speeds

--- src/main/java/net/beyond_horizons/items/ModToolMaterials.java
+++ src/main/java/net/beyond_horizons/items/ModToolMaterials.java
@@ -16,25 +16,25 @@ import java.util.function.Supplier;
 
 public enum ModToolMaterials implements ToolMaterial {
 
-    Ruby(ModTags.Blocks.INCORRECT_FOR_RUBY_TOOL, 784, 3, 2F, 21, () ->
+    Ruby(ModTags.Blocks.INCORRECT_FOR_RUBY_TOOL, 784, 12.5F, 2F, 21, () ->
             Ingredient.ofItems(ModItems.RUBY)),
 
-    Enderite(ModTags.Blocks.INCORRECT_FOR_ENDERITE_TOOL, 1974, 4, 2.5F, 27, () ->
+    Enderite(ModTags.Blocks.INCORRECT_FOR_ENDERITE_TOOL, 1974, 10.5F, 2.5F, 27, () ->
             Ingredient.ofItems(ModItems.ENDERITE_INGOT)),
 
-    Phoenixite(ModTags.Blocks.INCORRECT_FOR_PHOENIXITE_TOOL, 1841, 4, 3.0F, 27, () ->
+    Phoenixite(ModTags.Blocks.INCORRECT_FOR_PHOENIXITE_TOOL, 1841, 10.0F, 3.0F, 27, () ->
             Ingredient.ofItems(ModItems.PHOENIXITE_INGOT)),
 
-    Celestite(ModTags.Blocks.INCORRECT_FOR_CELESTITE_TOOL, 2133, 4, 3.0F, 27, () ->
+    Celestite(ModTags.Blocks.INCORRECT_FOR_CELESTITE_TOOL, 2133, 11.0F, 3.0F, 27, () ->
             Ingredient.ofItems(ModBlocks.CELESTITE_BLOCK)),
 
-    Cobalt(ModTags.Blocks.INCORRECT_FOR_COBALT_TOOL, 1306, 4, 2.0F, 24, () ->
+    Cobalt(ModTags.Blocks.INCORRECT_FOR_COBALT_TOOL, 1306, 10.0F, 2.0F, 24, () ->
             Ingredient.ofItems(ModItems.COBALT_INGOT)),
 
-    Mythril(ModTags.Blocks.INCORRECT_FOR_MYTHRIL_TOOL, 1923, 4, 2.0F, 24, () ->
+    Mythril(ModTags.Blocks.INCORRECT_FOR_MYTHRIL_TOOL, 1923, 10.0F, 2.0F, 24, () ->
             Ingredient.ofItems(ModItems.MYTHRIL_INGOT)),
 
-    CobaltMT(ModTags.Blocks.INCORRECT_FOR_COBALTMT_TOOL, 5000, 5, 3.0F, 27, () ->
+    CobaltMT(ModTags.Blocks.INCORRECT_FOR_COBALTMT_TOOL, 5000, 13F, 3.0F, 27, () ->
             Ingredient.ofItems(ModBlocks.COBALT_BLOCK));
 
     private final TagKey<Block> inverseTag;
